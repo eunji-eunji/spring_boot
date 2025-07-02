5cfe51e2fe8eb924843052fb48e6e6c266f9642c
limur-kr <ehdrms4459@naver.com>
2025-07-02 10:02:02 +0900

    application.properties 수정 사항 커밋

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Product { // 상품
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String description;
+    private int price;
+
+    private String category; // 예: 가구, 침대, 조명 등
+
+    private int stockQuantity; // 전체 재고 수량
+    private double averageRating; // 후기 평점 평균
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductModel> productModels = new ArrayList<>();
+
+    public void addProductModel(ProductModel model) {
+        this.productModels.add(model);
+        model.setProduct(this);
+    }
+}
