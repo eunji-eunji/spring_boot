1896248d4fab46e34ff63f54a1d043084c995af5
eousink01 <eousink01@gmail.com>
2025-07-09 21:26:14 +0900

    first commit

--- 파이널프로젝트/wooriZip-수정중/src/main/java/com/example/demo/repository/OrderDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Long> {
-
-}
