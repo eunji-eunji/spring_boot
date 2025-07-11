eff4f37de819b70095f151a11ddb9742c865c20b
eousink01 <eousink01@gmail.com>
2025-07-11 11:56:24 +0900

    s

---
+++ wooriZip-수정중/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.Users;
+import com.example.demo.security.CustomUserDetails;
+import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String singupForm() {
+        return "/user/signUp";
+    }
+
+    @PostMapping("signup")
+    public String signUp(@ModelAttribute UserDto dto){
+        userService.signUp(dto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        model.addAttribute("loginUser", userDetails.getUser()); // Users 엔티티 전달
+        return "/user/mypage";
+    }
+
+    @GetMapping("/edit")
+    public String editForm(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        Users user = userService.findById(userDetails.getId());
+        model.addAttribute("loginUser", user);
+        return "/user/edit";
+    }
+
+    @PostMapping("/edit")
+    public String editInfo(@ModelAttribute UserDto dto,
+                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+        userService.edit(dto, userDetails.getId());
+        return "redirect:/mypage";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        userService.delete(userDetails.getId());
+        return "redirect:/logout"; // Spring Security logout 처리 URL
+    }
+
+
+}
